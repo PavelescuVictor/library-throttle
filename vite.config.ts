ed117431fa20d7efb63e09749fa9ce62993eceb0
Victor Pavelescu <victor.pavelescu@endava.com>
2024-10-25 15:58:57 +0300

    feat: add vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            name: 'library-classnames',
+            fileName: (format) => `library-debounce.${format}.js`,
+            formats: ['es', 'umd'],
+        },
+        rollupOptions: {
+            input: 'src/index.ts',
+        }
+    },
+    server: {
+        open: true,
+    },
     test: {
         setupFiles: './src/throttle.test.ts',
     }
